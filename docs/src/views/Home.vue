628a762d131be1d1f3932ee3f09f464fcb49ca37
leifriksheim <riksheim.leif@gmail.com>
2020-04-22 08:50:50 -0600

    update front page

--- docs/src/views/Home.vue
+++ docs/src/views/Home.vue
@@ -1,20 +1,61 @@
 <template>
   <Page class="landing">
-    <div class="content">
-      <h1>
-        <b>Base Elements</b> is a
-        free collection of essential components for any web application
-      </h1>
-      <p class="lead">
+    <base-grid columns="12" gutter="md" gutter-md="xl">
+    <base-grid-item sm="12" md="5">
+      <base-text tag="h1" weight="400">
+        <b>Base Elements</b> is a free collection of essential UI components for
+        any web application
+      </base-text>
+      <base-text tag="p" class="lead">
         Components that work in
-        <b>React</b>,
-        <b>Vue</b>,
-        <b>Angular</b>, or any
-        other framwork.
-      </p>
-      <base-button type="primary">Get Started</base-button>
-      <base-button outline type="primary">The Components</base-button>
-    </div>
+        <b>React</b>, <b>Vue</b>, <b>Angular</b>, or any other framwork.
+      </base-text>
+      <router-link tag="base-button" to="/installation" type="primary">
+      Get Started
+      </router-link>
+      <router-link tag="base-button" outline to="/components" type="primary">
+        Components
+      </router-link>
+    </base-grid-item>
+
+    <base-grid-item sm="12" md="7">
+
+      <base-box depth="md" padding="lg" radius="lg">
+
+      <base-text tag="h2">Examples</base-text>
+
+      <base-grid columns="2">
+        <base-grid-item sm="6">
+          <base-select placeholder="Select an option">
+            <base-option label="Option 1"></base-option>
+            <base-option label="Option 2"></base-option>
+          </base-select>
+        </base-grid-item>
+        <base-grid-item>
+          <base-checkbox size="sm">Option 1</base-checkbox>
+          <base-checkbox size="sm">Option 2</base-checkbox>
+        </base-grid-item>
+        <base-grid-item>
+          <base-input placeholder="Regular input"></base-input>
+        </base-grid-item>
+        <base-grid-item>
+          <base-radio size="sm" name="radio">Option 1</base-radio>
+          <base-radio size="sm" name="radio">Option 2</base-radio>
+          <base-radio size="sm" name="radio">Option 3</base-radio>
+        </base-grid-item>
+        <base-grid-item>
+          <base-modal @toggle="e => modalOpen = e.open" :open="modalOpen">
+            <header slot="header">Title</header>
+            <div>Content</div>
+          </base-modal>
+          <base-button size="sm" type="primary" @click="modalOpen = true">Show modal</base-button>
+        </base-grid-item>
+      </base-grid>
+
+      </base-box>
+
+    </base-grid-item>
+    <base-grid>
   </Page>
 </template>
 
@@ -22,7 +63,12 @@
 import Page from "../layouts/Page";
 
 export default {
-  components: { Page }
+  components: { Page },
+  data() {
+    return {
+      modalOpen: false,
+    }
+  }
 };
 </script>
 
